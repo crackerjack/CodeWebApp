9d43a63f27fdccc8c61fcea6ca2bdab8d6bd4474
crackerjack <lance.h.obson@gmail.com>
2016-05-23 12:53:30 +1000

    Added default Listening port if running locally

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello from DBYD & Azure Web Apps!</h1>');
-}).listen(process.env.PORT);
+    res.end('<h1>Hello from local & Azure Web Apps!</h1>');
+}).listen(process.env.PORT || 8080, function () {
+  console.log('Node listening on port 8080!');
+});
