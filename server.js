fd2849a34a4a014e831976a2de716787d6d81306
crackerjack <lance.h.obson@gmail.com>
2016-05-24 16:00:27 +1000

    Attempt to Push Express to Azure

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log('Got request for ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello from Lance & Azure Web Apps!</h1>');
-}).listen(process.env.PORT || 8080, function () {
-  console.log('Node listening on port 8080!');
-});
