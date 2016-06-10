9414f874273fd551c5f856d62d99edbd3c132102
Hobson <lance.hobson@au.fujitsu.com>
2016-06-10 13:14:43 +1000

    Username should be passed as LowerCase

--- passport/login.js
+++ passport/login.js
@@ -9,15 +9,15 @@ module.exports = function (passport) {
     },
         function (req, username, password, done) {
             // check if a user with username exists or not            
-            User.findOne(username).then(function (user) {
+            User.findOne(username.toLowerCase()).then(function (user) {
                 if (!user) {
-                    console.log('User Not Found with username ' + username);
-                    return done(null, false, req.flash('message', 'User Not found.'));
+                    console.log('User Not Found with username ' + username.toLowerCase());
+                    return done(null, false, req.flash('message', 'User and Password not matched.'));
                 }
                 // User exists but wrong password, log the error 
                 if (!User.isValidPassword(user, password)) {
                     console.log('Invalid Password');
-                    return done(null, false, req.flash('message', 'Invalid Password')); // redirect back to login page
+                    return done(null, false, req.flash('message', 'User and Password not matched')); // redirect back to login page
                 }
                 // User and password both match, return user from done method
                 // which will be treated like success
